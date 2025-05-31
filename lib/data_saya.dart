7c3ab255f3114f99ab7969cb0a7ed8ec7f157447
unknown <aliaintan2404@gmail.com>
2025-05-31 20:41:52 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,5 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Alia";
+  static String gambar =
+      "assets/mahasiswa/alia intan syaharani.jpeg";
 }
